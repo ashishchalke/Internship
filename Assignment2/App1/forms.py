fd351fdc088da5b20f83636ec288ccc4d17d7785
ashishchalke <ashishchalke813@gmail.com>
2025-10-16 04:34:30 +0530

    Delete Assignment2 directory

--- Assignment2/App1/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Formsmodel
-
-class Formsform(forms.ModelForm):
-    class Meta:
-        model = Formsmodel
-        fields = "__all__"
--- Assignment2/App1/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Formsmodel(models.Model):
-    SrNo = models.CharField(max_length=20,unique=True)
-    Role_Name = models.CharField(max_length=50)
-    Role_Description = models.CharField(max_length=100)
